ee2c01d4a4b45b28f21366d3ae7373adf85ede25
Sassmit Laal <slaal@C02CQ3A1MD6N.ad.here.com>
2024-12-13 03:51:23 +0530

    add task icon

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import "./sidebar.css";
 import { assets } from "../../assets/assets";
 import { useContext, useState } from "react";
 import { Context } from "../../context/Context";
-import TaskScheduler from "../taskScheduler/TaskScheduler.jsx";  // Import TaskScheduler component
+import TaskScheduler from "../taskScheduler/TaskScheduler"; // Import TaskScheduler component
 
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
@@ -73,9 +73,20 @@ const Sidebar = () => {
           <img src={assets.setting_icon} alt="" />
           {extended ? <p>Settings</p> : null}
         </div>
-        {/* New Task Scheduler button */}
+        {/* Task Scheduler button */}
         <div className="bottom-item recent-entry" onClick={toggleTaskScheduler}>
-          <img src={assets.task_icon} alt="task-icon" /> {/* Add a task icon */}
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 256 256"
+            className="tasks-icon"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M172.24,99.76a6,6,0,0,1,0,8.48l-56,56a6,6,0,0,1-8.48,0l-24-24a6,6,0,0,1,8.48-8.48L112,151.51l51.76-51.75A6,6,0,0,1,172.24,99.76ZM230,128A102,102,0,1,1,128,26,102.12,102.12,0,0,1,230,128Zm-12,0a90,90,0,1,0-90,90A90.1,90.1,0,0,0,218,128Z"></path>
+          </svg>
           {extended ? <p>Task Scheduler</p> : null}
         </div>
       </div>
